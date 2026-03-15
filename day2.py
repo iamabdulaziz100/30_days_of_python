a18accbb31bf1a69d63a8f75e4dea92e688ba6e6
Abdul Aziz <iamabdulaziz100@gmail.com>
2026-03-15 11:17:05 +0600

    day 2 finish

--- day2.py
+++ day2.py
@@ -1,10 +1,36 @@
+from math import pi
+
 # Day 2: 30 Days of pyhton programing
 
-first_name = "Abdul"
-last_name = "Aziz"
-Full_name = "Abdul Aziz"
-contry = "Bangladesh"
-city = "Noakhali"
-age = 23
-is_married = False
-do_love_girls = True
+# first_name = "Abdul"
+# last_name = "Aziz"
+# Full_name = "Abdul Aziz"
+# contry = "Bangladesh"
+# city = "Noakhali"
+# age = 23
+# is_married = False
+# name, age, location = "Abdul Aziz", 22, "Bangladesh"
+#
+# print(type(is_married), type(city))
+# print(len(first_nam))
+#
+# num_one, num_two = 5,4
+# total = num_one + num_two
+# diff = num_one - num_two
+# reminder = num_one / num_two
+# exp = num_one ** num_two
+# floor_division = num_one // num_two
+
+# radius = int(input("What is the raidus or the circle: "))
+#
+# area_of_circle = pi * (radius**2)
+#
+# print(area_of_circle)
+#
+# circum_of_circle = 2 * pi * radius
+
+
+first_name = input("Your first name? ")
+# last_name = input("Your last name? ")
+
+print(first_name)
