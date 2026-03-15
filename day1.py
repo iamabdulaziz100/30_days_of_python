662ebde8b387c1eaf165b5a109b1280422dc1677
Abdul Aziz <iamabdulaziz100@gmail.com>
2026-03-15 10:19:03 +0600

    day 1 & 2

---
+++ day1.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+
+print(1 + 2)
+print(2 - 2)
+print(2 * 2)
+print(6 / 2)
+print(3 ** 2)
+print(3 % 2)
+print(3 // 2)
+
+
+print(type(10))
---
+++ day2.py
@@ -0,0 +1,10 @@
+# Day 2: 30 Days of pyhton programing
+
+first_name = "Abdul"
+last_name = "Aziz"
+Full_name = "Abdul Aziz"
+contry = "Bangladesh"
+city = "Noakhali"
+age = 23
+is_married = False
+do_love_girls = True
