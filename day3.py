698aee8d6d4add4fd1aca29371fd356e30e1493b
Abdul Aziz <iamabdulaziz100@gmail.com>
2026-03-15 12:19:28 +0600

    day 3 finish

---
+++ day3.py
@@ -0,0 +1,48 @@
+age = 20
+height = 5.5
+complex_number = 2 + 3j
+
+#script that prompts the user to enter base and height of the triangle and calculate an area of this triangle
+# base = float(input("Enter base: "))
+# height = float(input("Enter height: "))
+# area = 0.5 * base * height
+# print(area)
+
+#Find the length of 'python' and 'dragon' and make a falsy comparison statement
+# print(len("python") > len("dragon"))
+# print(len("python") < len("dragon"))
+# print(len("python") != len("dragon"))
+
+# if "on" in "python" and "dragon":
+#     print("yes")
+
+
+# if "on" not in "python" and "dragon":
+#     print("True")
+# else:
+#     print("False")
+
+
+# print(type(str(float(len("python")))))
+
+
+# def is_even(num):
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Not Even")
+#
+# is_even(5)
+
+# if (7 // 2) == int(2.7):
+#     print("yes")
+# else:
+#     print("No")
+
+
+# years = int(input("Enter number of years you have lived: "))
+# print(f"you have lived for {years * 31536000} Seconds.")
+
+n_rows = 5
+for n in range(1, n_rows+1):
+    print(n, 1, n, n**2, n**3)
