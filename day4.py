530386e40d8bc960480836ce563129fe69738f8c
Abdul Aziz <iamabdulaziz100@gmail.com>
2026-03-16 11:48:48 +0600

    day 4 finish

---
+++ day4.py
@@ -0,0 +1,24 @@
+# string_concatenation = "Thirty " + "Days " + "of " + "Python"
+# # print(string_concatenation)
+# print(len(string_concatenation))
+# print(string_concatenation.upper())
+# print(string_concatenation.lower())
+
+# print("Coding for All".capitalize())
+# print("Coding for All".title())
+# print("Coding for All".swapcase())
+
+# print("Coding For All"[:6])
+
+# print("Coding For All".find("Coding"))
+
+# print("Coding For All".replace("Coding", "Running"))
+
+# print("Coding For All".split())
+
+# print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(","))
+
+# s = "Coding For All"
+# print(s[0])
+
+# print(len("Coding For All") - 1)
